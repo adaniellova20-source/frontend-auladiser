0e049bb3d7d2d81f2544528ba47ccda0765253d0
adaniellova20-source <adaniellova20@gmail.com>
2025-11-21 10:36:36 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import App from './App';
+
+const element = document.getElementById('root');
+
+const root = ReactDOM.createRoot(element);
+
+root.render(
+    <App/>
+);
