5d474ae860d094442abaa8b820fe7161f5692e5d
adaniellova20-source <adaniellova20@gmail.com>
2026-03-23 12:54:42 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const element = document.getElementById('root');
+import App from "./App";
 
-const root = ReactDOM.createRoot(element);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <App/>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
